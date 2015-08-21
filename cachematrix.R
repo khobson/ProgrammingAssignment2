87d35f7f7e9583d62c0381d75f294d2f126578cf
Kristian Hobson <khobson@4metascape.com.au>
2015-08-21 14:14:05 +1000

    K Hobson
    Kristian Hobson r Programming Assignment 2 Contribution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Kristian Hobson. r Programming Assignment 2, 21/08/2015
+##
+## Included are 2 functions for the purposes of this assignment. 
+##			makeCacheMatrix
+##			cacheSolve
+##
+##makeCacheMatrix - 	This function commits a value to an object in an environment that is different to the environment we are 
+##			currently in. In this case a matrix.
+##
+##cacheSolve -		This function inverts a matrix and returns the inverted from that submitted to the function
+##
+##
+##
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	## Set the variable to null.
+        invert = NULL
+        set = function(y) {
+                ## use `<<-` to assign a value  
+                x <<- y
+		## reset the invert variable to null.
+                invert <<- NULL
+        }
+        get = function() x
+        setinv = function(inverse) invert <<- inverse 
+        getinv = function() invert
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## This function inverts the input variable, as a matrix
+        
+	## Establish the variable with the data passed in
+        invert = x$getinv()
+        
+        ## if we have already done it, do not do it again
+        if (!is.null(invert)){
+                ## Send a message back to the console. 
+                message("getting cached data")
+
+		## Return out of the function without doing it again
+                return(invert)
+        }
+        
+        ## If we have not done it, as above, prepare using the solve function as advised
+        mat.data = x$get()
+        invert = solve(mat.data, ...)
+        
+        ## Invert the data using the setinv function.
+        x$setinv(invert)
+        
+	## Finally return the data.
+        return(invert)
 }
